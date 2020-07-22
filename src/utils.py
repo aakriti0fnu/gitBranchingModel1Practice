254a2a373593676a8a6eb685b7499da76c68b3d5
aakriti0fnu <aakriti.cse@gmail.com>
2020-07-22 14:28:38 -0700

    added utility string to boolean conversion.

--- src/utils.py
+++ src/utils.py
@@ -16,3 +16,14 @@ def timeit(method):
             print("{} took {}.".format(method.__name__,time.strftime("%H:%M:%S" , time.gmtime((te - ts))) ))
         return result
     return timed
+
+def str2bool(v):
+    if isinstance(v, bool):
+       return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError("Boolean value expected. ['yes', 'true', 't', 'y', '1', 'no', 'false', 'f', 'n', '0']" )
+

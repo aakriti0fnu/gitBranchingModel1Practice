d73e539837580cd4b394d609493e5c368c4bdd39
aakriti0fnu <aakriti.cse@gmail.com>
2020-07-22 20:34:21 -0700

    added a decorator to debug a function call

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import time
 import argparse
 from datetime import datetime, timezone
+import functools
 
 def timeit(method):
     def timed(*args, **kw):
@@ -32,3 +33,16 @@ def str2bool(v):
         return False
     else:
         raise argparse.ArgumentTypeError("Boolean value expected. ['yes', 'true', 't', 'y', '1', 'no', 'false', 'f', 'n', '0']" )
+
+def debug(func):
+    """Print the function signature and return value"""
+    @functools.wraps(func)
+    def wrapper_debug(*args, **kwargs):
+        args_repr = [repr(a) for a in args]                      # 1
+        kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]  # 2
+        signature = ", ".join(args_repr + kwargs_repr)           # 3
+        print(f"Calling {func.__name__}({signature})")
+        value = func(*args, **kwargs)
+        print(f"{func.__name__!r} returned {value!r}")           # 4
+        return value
+    return wrapper_debug

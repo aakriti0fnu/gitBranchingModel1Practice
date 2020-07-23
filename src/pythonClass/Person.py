9df210929b0bd4313da097a4632454d5b4b7d290
aakriti0fnu <aakriti.cse@gmail.com>
2020-07-22 22:51:33 -0700

    implementing __str__ and __repr__ for debugging purpose.

--- src/pythonClass/Person.py
+++ src/pythonClass/Person.py
@@ -6,36 +6,40 @@ class Person:
         self.name = personName
         self.age = personAge
 
-    # def __repr__(self):
-    #     '''
-    #
-    #     :return: the object representation
-    #     '''
-    #     return {'name':self.name, 'age':self.age}
-    #
-    # def __str__(self):
-    #     '''
-    #     it's called when print() or str() functions are invoked on an object.
-    #     :return: the string representation of the object
-    #     '''
-    #     return 'Person(name='+self.name+', age='+str(self.age)+ ')'
+    def __repr__(self):
+        '''
+
+        :return: the object representation
+        '''
+        return {'name':self.name, 'age':self.age}
+
+    def __str__(self):
+        '''
+        it's called when print() or str() functions are invoked on an object.
+        :return: the string representation of the object
+        '''
+        return 'Person(name='+self.name+', age='+str(self.age)+ ')'
 
 if __name__ == '__main__':
 
     p = Person('Pankaj', 34)
 
-    print(p.__str__()) # <__main__.Person object at 0x7ff6610f0690>
-    print(p.__repr__()) # <__main__.Person object at 0x7ff6610f0690>
+    # print(p.__str__()) # <__main__.Person object at 0x7ff6610f0690>
+    # print(p.__repr__()) # <__main__.Person object at 0x7ff6610f0690>
     # As you can see above that the default implementation is useless!
     print('`'*5)
-    # # __str__() example
-    # print(p)
-    # print(p.__str__())
-    #
-    # s = str(p)
-    # print(s)
-    #
-    # # __repr__() example
-    # print(p.__repr__())
-    # print(type(p.__repr__()))
-    # print(repr(p))
+    # __str__() example
+    print(p)
+    print(p.__str__())
+    print('`'*5)
+
+    s = str(p)
+    print(s)
+
+    # __repr__() example
+    print(p.__repr__())
+    print(type(p.__repr__()))
+    print('`'*5)
+
+    print(repr(p)) # trows TypeError! since our __repr__ impl. returns a {} rather than a str
+    
